505f8f9e32637d98dd68763182c70b2434d2f3ef
nekisse <lsh891224@gmail.com>
2019-01-13 16:40:59 +0900

    wirebarley 저장소 생성.

---
+++ src/main/java/com/nekisse/wirebarley/wirebarleyinterview/WirebarleyinterviewApplication.java
@@ -0,0 +1,14 @@
+package com.nekisse.wirebarley.wirebarleyinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirebarleyinterviewApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WirebarleyinterviewApplication.class, args);
+  }
+
+}
+
